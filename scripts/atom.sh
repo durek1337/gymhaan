77c988babdc2075391a3c1a347aac1337193958f
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-02-29 00:48:03 +0100

    Atom script install

---
+++ scripts/atom.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:webupd8team/atom
+sudo apt update; sudo apt install atom
+apm install atom-beautify atom-bracket-highlight autocomplete-modules file-icons git-plus highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view platformio-ide-terminal teletype  
