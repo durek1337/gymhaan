decd7c17391f2c7115ac64fe689039108890520a
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-02-29 03:32:29 +0100

    update

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -4,4 +4,5 @@ sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
 sudo apt install atom
-apm install busy-signal atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view teletype atom-ide-terminal
+apm install busy-signal atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view atom-ide-terminal
+sudo apm install teletype
