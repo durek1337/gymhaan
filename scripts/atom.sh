ca5d0fc41454c1979decf250b222aa453f4f11b8
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-02-29 02:06:15 +0100

    update

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -1,3 +1,6 @@
-sudo add-apt-repository ppa:webupd8team/atom
-sudo apt update; sudo apt install atom
-apm install atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view platformio-ide-terminal@2.8.4 teletype  
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+sudo apt install atom
+apm install atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view platformio-ide-terminal@2.8.4 teletype
