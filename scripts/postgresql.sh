eda5bcf638b5364e47b3d10aa1a854ffd5c06062
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-02-29 03:40:18 +0100

    update

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -2,4 +2,4 @@
 sudo apt-get install curl ca-certificates gnupg
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-12
+sudo apt-get install postgresql-11
