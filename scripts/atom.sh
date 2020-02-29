8693e1bb94fedf511266a27bf9dabdc1a76abe78
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-02-29 01:36:15 +0100

    git-plus aus apm weg und platform-ide-terminal@2.8.4 ersetzt

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -1,3 +1,3 @@
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt update; sudo apt install atom
-apm install atom-beautify atom-bracket-highlight autocomplete-modules file-icons git-plus highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view platformio-ide-terminal teletype  
+apm install atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view platformio-ide-terminal@2.8.4 teletype  
