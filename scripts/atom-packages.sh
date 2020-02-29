c587f302020907d8e1dd00d8603d60c73a9629a5
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-02-29 19:51:06 +0100

    update

---
+++ scripts/atom-packages.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+apm install busy-signal atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view teletype platformio-ide-terminal
+# Package platformio-ide-terminal macht für einige Versionen Probleme
--- scripts/startscript.sh
+++ scripts/startscript.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ./basic.sh
 ./atom.sh
+./atom-packages.sh
 #./dbeaver.sh
 #./openscad.sh
 #./postgresql.sh
