8dce6db97da0c613b23a54a351215c71a179c019
Dominik Höltgen <dominik.hoeltgen@uni-wuppertal.de>
2020-03-01 15:20:51 +0100

    Leerzeilen entfernt

--- scripts/atom-packages.sh
+++ scripts/atom-packages.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-
-
-
-
 apm install platformio-ide-terminal busy-signal atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view teletype wordcount hyperclick atom-ternjs autocomplete-sql autocomplete-python
