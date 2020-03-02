4144cc39f0e34932bcb0e71bf04e72d652bf762d
LAuch_mit_BAuch <lauch1900@gmail.com>
2020-03-02 22:23:24 +0100

    tool to write code together

--- scripts/atom-packages.sh
+++ scripts/atom-packages.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-apm install platformio-ide-terminal busy-signal atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view teletype wordcount hyperclick atom-ternjs autocomplete-sql autocomplete-python pigments autoclose-html-plus
+apm install platformio-ide-terminal busy-signal atom-beautify atom-bracket-highlight autocomplete-modules file-icons highlight-selected intentions language-babel linter linter-eslint linter-htmlhint language-latex linter-ui-default minimap pdf-view teletype wordcount hyperclick atom-ternjs autocomplete-sql autocomplete-python pigments autoclose-html-plus teletype
