7e18227af6d0016d08e1cb049edc0898124480ee
EinSackZwiebeln <41073131+Sheesher@users.noreply.github.com>
2020-03-03 21:36:32 +0100

    Create zsh.sh
    zsh ist besser als bash

---
+++ scripts/zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt install zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  
+
+# ggf. noch p10k, dafür braucht man aber kein sudo und es ist ne geschmackssache
